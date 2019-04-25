a31cb5996cce4d11bfa4d9584b774e9538df4476
Kacper Makuch <kacbull007@gmail.com>
2019-04-26 01:30:11 +0200

    Pack in method

--- mushroom_classification.py
+++ mushroom_classification.py
@@ -5,16 +5,21 @@ import numpy as np
 
 data_path = 'mushroom_data/mushroom.data'
 
-#def data_set_to_tensor
-dataset = tf.data.experimental.CsvDataset(
-        data_path,
-        np.full(22,tf.string),
-        select_cols=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
-        )
+def data_set_to_tensor(verbose=0):
+    dataset = tf.data.experimental.CsvDataset(
+            data_path,
+            np.full(22,tf.string),
+            select_cols=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+            )
 
-i = 0
-for ele in dataset:
-    i += 1
-    print(ele)
-print(i)
-print(type(dataset))
+    i = 0
+    for element in dataset:
+        i += 1
+        if (verbose == 1):
+            print(element) 
+    print(i,'data records')
+    return dataset
+
+if (__name__ == '__main__'):
+    #data_set_to_tensor()
+
