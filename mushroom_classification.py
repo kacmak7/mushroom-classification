6bb59b0ffb50680b47418e4953fa78471bd69088
Kacper Makuch <kacbull007@gmail.com>
2019-05-05 20:30:21 +0200

    Successfully build mushrroom_classification.py

--- mushroom_classification.py
+++ mushroom_classification.py
@@ -1,7 +1,8 @@
-import pandas
+import pandas as pd
 import tensorflow as tf
 import numpy as np
 
+"""
 tf.enable_eager_execution()
 data_path = 'mushroom_data/mushroom.data'
 
@@ -38,3 +39,45 @@ if (__name__ == '__main__'):
    #         print(value)
    #     except tf.errors.OutOfRangeError:
    #         break
+"""
+
+
+data_path = 'mushroom_data/mushroom.data'
+
+def generate_data_array():
+    df = pd.read_csv(data_path)
+    data = pd.get_dummies(df).to_numpy()
+    print('data\n',data)
+    return data
+
+if (__name__ == '__main__'):
+    data = generate_data_array()
+
+    split = int(data.shape[0]/2)
+    print(split)
+    X_train = data[:split,1:]
+    y_train = data[:split,:1]
+    print('X_train\n',X_train)
+    print('y_train\n',y_train)
+    X_test = data[split:,1:]
+    y_test = data[split:,:1]
+    print('X_test\n',X_test)
+    print('y_test\n',y_test)
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(2,activation=tf.nn.softmax))
+    model.compile(optimizer='adam',loss='mean_squared_error',metrics=['accuracy'])
+    model.fit(X_train, y_train, epochs=10)
+
+    predictions = model.predict(X_test)
+
+    matches = 0
+    for i in range(np.size(predictions,0)):
+        x = int(np.argmax(predictions[i-1]))
+        y = int(y_test[i-1][0])
+        #print(x,y)
+        if (x == y):
+            matches += 1
+    print(matches/int(np.size(predictions,0))*100,'%')
+
