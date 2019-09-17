e929a3d3f9aceebe0772c8a3e123c84ff5158b65
Kacper Makuch <kacbull007@gmail.com>
2019-09-17 22:55:34 +0200

    Proceed with research

--- python-mushroom-classification/research/exam/exam.py
+++ python-mushroom-classification/research/exam/exam.py
@@ -26,7 +26,9 @@ class Neural_Network:
 
     def forward(self, X):
         self.hidden = np.dot(X,self.weights1)
+        print(self.hidden)
         self.activated_hidden = self.sigmoid(self.hidden)
+        print()
         self.output = np.dot(self.activated_hidden,self.weights2)
         self.activated_output = self.sigmoid(self.output)
         print(self.activated_hidden)
@@ -35,16 +37,16 @@ class Neural_Network:
 
     def backward(self,X,y,output):
         self.output_error = y - output
-        self.output_delta = self.output_error*self.sigmoidPrime(o)
+        self.output_delta = self.output_error*self.sigmoidPrime(output)
 
-        self.hidden_error = self.o_delta.dor(self.weights2.T)
+        self.hidden_error = self.output_delta.dot(self.weights2.T)
         self.hidden_delta = self.hidden_error*self.sigmoidPrime(self.hidden)
 
         self.weights1 += X.T.dot(self.hidden_delta)
         self.weights2 += self.hidden.T.dot(self.output_delta)
     
     def train(self, X, y, epochs = 100):
-        for i in range(100):
+        for i in range(epochs):
             output = self.forward(X)
             self.backward(X, y, output)
 
@@ -53,3 +55,15 @@ class Neural_Network:
 
     def sigmoidPrime(self,s):
         return s*(1-s)
+
+nn = Neural_Network()
+nn.train(X, y)
+
+#test 
+nn.forward(X)
+print('input')
+print(X)
+print('output')
+print(nn.output)
+print('expected output')
+print(y)
